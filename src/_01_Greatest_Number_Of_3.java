9e127c739875a1e2d579ab655e37d625355c0718
akinkayagithub <akinkaya.b@gmail.com>
2022-02-11 09:59:35 -0600

    added new ways to find greatest of int numbers

--- src/_01_Greatest_Number_Of_3.java
+++ src/_01_Greatest_Number_Of_3.java
@@ -1,3 +1,6 @@
+import java.util.Arrays;
+import java.util.TreeSet;
+
 public class _01_Greatest_Number_Of_3 {
     /*
     Write a method that takes 3 int arguments and returns the greatest number
@@ -18,10 +21,31 @@ public class _01_Greatest_Number_Of_3 {
      */
 
     //WAY 2: Use if else statements to find max value of 3 int variables as below
+    /*
     public static int getGreatest(int a, int b, int c){
         if(a >= b && a >= c) return a;
         else if(b >= a && b >= c) return b;
         return c;
+    }
+     */
+
+    //WAY 3: Store the given ints in a TreeSet and return the last element
+    /*
+    public static int getGreatest(int a, int b, int c){
+        TreeSet<Integer> numbers = new TreeSet<>();
+        numbers.add(a);
+        numbers.add(b);
+        numbers.add(c);
+
+        return numbers.last();
+    }
+     */
+
+    //WAY 4: Store the given ints in an array and return the last index of the array after sorting it
+    public static int getGreatest(int a, int b, int c){
+        int[] numbers = {a, b, c};
+        Arrays.sort(numbers);
+        return numbers[numbers.length-1];
     }
 
 
